6ca10118b85e3369ade5b6c818bb7ccc7a73ba9c
crowcrow07 <skathd3402@gmail.com>
2023-05-09 10:53:39 +0900

    app.js에 컴포넌트 임포트 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListSide from "./component/ListSide";
+import Nav from "./component/Nav";
+import InputSide from "./component/InputSide";
+import data from "./data/data";
+import { useState } from "react";
 
 function App() {
+  const [dataes, setDataes] = useState(data);
+
+  const buttonHandler = (inputStr) => {
+    setDataes([
+      ...dataes,
+      { id: dataes.length + 1, text: inputStr, checked: false },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col">
+      <div className="flex justify-center">
+        <Nav />
+      </div>
+      <div className="h-12" />
+      <div className="flex flex-row h-128 p-8">
+        <ListSide dataes={dataes} setDataes={setDataes} />
+        <InputSide buttonHandler={buttonHandler} />
+      </div>
     </div>
   );
 }
