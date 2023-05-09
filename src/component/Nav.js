33c9c60beca9c5add1adc09804761f61a60c438e
crowcrow07 <skathd3402@gmail.com>
2023-05-09 10:52:06 +0900

    nav바 생성

---
+++ src/component/Nav.js
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <div className="flex justify-center items-center pl-12 pr-12 w-11/12 h-36 text-4xl text-white border-b-2 ">
+      오늘 뭐하지
+    </div>
+  );
+}
